48ad80d3f89f099c32605448b90b8292178dfd4f
strombert <61539910+strombert@users.noreply.github.com>
2023-08-03 10:23:49 +0200

    1.0

---
+++ ticketreopen/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Ticket reopen on Customer Mail',
+    'version': '1.0',
+    'category': 'Helpdesk',
+    'depends': ['base', 'mail', 'helpdesk'],
+    'data': ['data/automated_action.xml'],
+}
